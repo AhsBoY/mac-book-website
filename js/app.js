749f3481be4b71efeac68be43c3079c38b6ce34f
AhsBoy <ahsanahmedr1000@gmail.com>
2021-08-21 21:12:18 +0600

    third

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ function getProductCost(product, price){
     productCost.innerText = updatedCost
     calculation ()
 }
-// get product total
+// get Product total
 function getTotal(product){
-    const extraCost = parseInt(document.getElementById(product + "-cost").innerText)
-    return extraCost 
+    const productCost = parseInt(document.getElementById(product + "-cost").innerText)
+    return productCost 
 }
 // Calculating Total Price
+const totalPrice = document.getElementById("updated-total-cost")
 function calculation (){
     const basePrice = getTotal("base")
     const memoryCost = getTotal("memory") 
@@ -18,15 +19,28 @@ function calculation (){
     const deliveryCost = getTotal("delivery") 
     const totalCost = basePrice + memoryCost + storageCost + deliveryCost;
     document.getElementById("total-price").innerText = totalCost
-    document.getElementById("updated-total-cost").innerText = totalCost
+}
+// Verifying Promo Code
+function promoCode(){
+    const verifyCode = document.getElementById("code-area");
+    const givenCode = "stevekaku"
+    if(verifyCode.value == givenCode){
+        const updateTotal = parseInt(totalPrice.innerText) - parseInt(totalPrice.innerText) * 0.2;
+        totalPrice.innerText = updateTotal;
+    }
+    verifyCode.value = "";
 }
 
+// Handling Events
+// memory option
 document.getElementById("memory-slecetion-first").addEventListener("click", function(){
     getProductCost("memory",0);
 })
 document.getElementById("memory-slecetion-second").addEventListener("click", function(){
     getProductCost("memory",180);
 })
+
+// storage option
 document.getElementById("storage-selection-first").addEventListener("click", function(){
     getProductCost("storage",0);
 })
@@ -36,9 +50,16 @@ document.getElementById("storage-selection-second").addEventListener("click", fu
 document.getElementById("storage-selection-third").addEventListener("click", function(){
     getProductCost("storage",180);
 })
+
+// delivery option
 document.getElementById("delivery-selection-first").addEventListener("click", function(){
     getProductCost("delivery",0);
 })
 document.getElementById("delivery-selection-second").addEventListener("click", function(){
     getProductCost("delivery",20);
+})
+
+// Promocode Option
+document.getElementById("apply-btn").addEventListener("click", function(){
+    promoCode()
 })
